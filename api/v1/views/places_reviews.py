9ab5bb092daf606608e201ed5081fc1cc6a98d39
Ibrahim-Naguib <ibrahim1991ahmed@gmail.com>
2024-04-29 15:11:41 +0000

    new commit

--- api/v1/views/places_reviews.py
+++ api/v1/views/places_reviews.py
@@ -15,9 +15,7 @@ def get_reviews(place_id):
     place = storage.get(Place, place_id)
     if place is None:
         abort(404)
-    reviews = []
-    for review in place.reviews:
-        reviews.append(review.to_dict())
+    reviews = [review.to_dict() for review in place.reviews]
     return jsonify(reviews)
 
 
@@ -26,9 +24,10 @@ def get_reviews(place_id):
 def get_review(review_id):
     """returns review by id"""
     review = storage.get(Review, review_id)
-    if review is None:
+    if review:
+        return jsonify(review.to_dict())
+    else:
         abort(404)
-    return jsonify(review.to_dict())
 
 
 @app_views.route('/reviews/<review_id>', methods=['DELETE'],
@@ -51,15 +50,15 @@ def post_review(place_id):
     if place is None:
         abort(404)
     if not request.get_json():
-        return make_response(jsonify({"error": "Not a JSON"}), 400)
+        abort(400, 'Not a JSON')
     review_data = request.get_json()
     if 'user_id' not in review_data:
-        return make_response(jsonify({"error": "Missing user_id"}), 400)
-    user = storage.get('User', review_data['user_id'])
+        abort(400, 'Missing user_id')
+    user = storage.get(User, review_data['user_id'])
     if user is None:
         abort(404)
     if 'text' not in review_data:
-        return make_response(jsonify({"error": "Missing text"}), 400)
+        abort(400, 'Missing text')
     review_data['place_id'] = place_id
     review = Review(**review_data)
     review.save()
@@ -71,13 +70,14 @@ def post_review(place_id):
 def put_review(review_id):
     """updates a review by id"""
     review = storage.get(Review, review_id)
-    if review is None:
+    if review:
+        if not request.get_json():
+            abort(400, 'Not a JSON')
+        for key, value in request.get_json().items():
+            if key not in ['id', 'user_id', 'place_id', 'created_at',
+                           'updated_at']:
+                setattr(review, key, value)
+        review.save()
+        return make_response(jsonify(review.to_dict()), 200)
+    else:
         abort(404)
-    if not request.get_json():
-        return make_response(jsonify({"error": "Not a JSON"}), 400)
-    for key, value in request.get_json().items():
-        if key not in ['id', 'user_id', 'place_id', 'created_at',
-                       'updated_at']:
-            setattr(review, key, value)
-    review.save()
-    return make_response(jsonify(review.to_dict()))
