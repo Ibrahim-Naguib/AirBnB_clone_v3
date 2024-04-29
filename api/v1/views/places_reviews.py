06d7dfc9c4718d80aa4dd61bdbe1ad306448c08b
Ibrahim-Naguib <ibrahim1991ahmed@gmail.com>
2024-04-29 14:53:17 +0000

    new commit

--- api/v1/views/places_reviews.py
+++ api/v1/views/places_reviews.py
@@ -8,11 +8,11 @@ from api.v1.views import app_views
 from flask import jsonify, abort, request, make_response
 
 
-@app_views.route('/places/<string:place_id>/reviews', methods=['GET'],
+@app_views.route('/places/<place_id>/reviews', methods=['GET'],
                  strict_slashes=False)
 def get_reviews(place_id):
     """returns all reviews"""
-    place = storage.get('Place', place_id)
+    place = storage.get(Place, place_id)
     if place is None:
         abort(404)
     reviews = []
@@ -21,33 +21,33 @@ def get_reviews(place_id):
     return jsonify(reviews)
 
 
-@app_views.route('/reviews/<string:review_id>', methods=['GET'],
+@app_views.route('/reviews/<review_id>', methods=['GET'],
                  strict_slashes=False)
 def get_review(review_id):
     """returns review by id"""
-    review = storage.get('Review', review_id)
+    review = storage.get(Review, review_id)
     if review is None:
         abort(404)
     return jsonify(review.to_dict())
 
 
-@app_views.route('/reviews/<string:review_id>', methods=['DELETE'],
+@app_views.route('/reviews/<review_id>', methods=['DELETE'],
                  strict_slashes=False)
 def delete_review(review_id):
     """deletes review by id"""
-    review = storage.get('Review', review_id)
+    review = storage.get(Review, review_id)
     if review is None:
         abort(404)
     review.delete()
     storage.save()
-    return jsonify({})
+    return make_response(jsonify({}), 200)
 
 
-@app_views.route('/places/<string:place_id>/reviews', methods=['POST'],
+@app_views.route('/places/<place_id>/reviews', methods=['POST'],
                  strict_slashes=False)
 def post_review(place_id):
     """creates a new review"""
-    place = storage.get('Place', place_id)
+    place = storage.get(Place, place_id)
     if place is None:
         abort(404)
     if not request.get_json():
@@ -70,7 +70,7 @@ def post_review(place_id):
                  strict_slashes=False)
 def put_review(review_id):
     """updates a review by id"""
-    review = storage.get('Review', review_id)
+    review = storage.get(Review, review_id)
     if review is None:
         abort(404)
     if not request.get_json():
